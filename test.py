2cc2f3c555923d175faacf9974a62d338b86c4af
two <dtcf@163.com>
2018-10-23 08:32:02 +0800

    init

---
+++ test.py
@@ -0,0 +1,31 @@
+# -*- endoding: utf-8 -*-
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from LinearRegression import LinearRegressor
+
+def test_linear_regression():
+    X = 2 * np.random.rand(100, 1)
+    y = 4 + 3 * X + np.random.randn(100, 1)
+
+    lr = LinearRegressor(method='gradient', gradient='batch')
+    lr.fit(X, y, debug=True)
+    test_X = np.array([[0], [2]])
+    pred_y = lr.predict(test_X)
+
+    print(lr.theta)
+
+    '''
+    we can know that by setting parameter debug=True, stochastic and mini-batch gradient
+    descent cost much less epochs to reach a relatively small norm of gradient vector, which
+    indicates that stochastic and mini-batch train faster than batch gradient descent.
+    '''
+    
+    # plt.plot(test_X, pred_y, 'r-')
+    # plt.plot(X, y, 'b,')
+    # plt.axis([0, 2, 0, 15])
+    # plt.savefig('linear_regression.png')
+
+if __name__ == '__main__':
+    test_linear_regression()
