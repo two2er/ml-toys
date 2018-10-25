4607c1d6b101b8d725b615806b4b0b2f30229bfc
two <dtcf@163.com>
2018-10-25 16:55:08 +0800

    regularized

--- RegularizedLinearRegression.py
+++ RegularizedLinearRegression.py
@@ -1,31 +1,24 @@
 # -*- endoding: utf-8 -*-
 
 import numpy as np
-import abc
 from LinearRegression import LinearRegressor
 
 class RegularizedRegressor(LinearRegressor):
     """
     Ridge(l2-norm) Lasso(l1-norm) Regularization
     """
-    def __init__(self, learning_rate = 1e-3, tolerance = 1e-3, alpha = 0.5):
+    def __init__(self, learning_rate = 1e-3, n_epoch=2000, alpha = 0.05):
         self.alpha = alpha
-        super(RegularizedRegressor, self).__init__(learning_rate, tolerance)
+        super(RegularizedRegressor, self).__init__(learning_rate, n_epoch)
     
     def training_method(self, X, y):
-        count = 0
-        while True:
-            # delete 2 / X.shape[0], otherwise the regularization item would dominate
-            gradient_vector =   X.T.dot(X.dot(self.theta) - y)\
-                                + self.regularized_item()
-
-            if np.linalg.norm(gradient_vector) < self.tolerance:
-                break
+        for epoch in range(self.n_epoch):
+            gradient_vector =   2 / X.shape[0] * X.T.dot(X.dot(self.theta) - y)\
+                                    + self.regularized_item()
             self.theta = self.theta - self.learning_rate * gradient_vector
     
-    @abc.abstractmethod
     def regularized_item(self):
-        pass
+        raise NotImplementedError()
 
 class RidgeLinearRegressor(RegularizedRegressor):
     """
@@ -46,9 +39,9 @@ class ElasticNetRegressor(RegularizedRegressor):
     combining l1-regularizaiton and l2-regularization
     r * l1 + (1-r) * l2
     """
-    def __init__(self, learning_rate = 1e-3, tolerance = 1e-3, alpha = 0.5, r = 0.5):
+    def __init__(self, learning_rate = 1e-3, n_epoch = 2000, alpha = 0.5, r = 0.5):
         self.r = r
-        super(ElasticNetRegressor, self).__init__(learning_rate, tolerance, alpha)
+        super(ElasticNetRegressor, self).__init__(learning_rate, n_epoch, alpha)
 
     def regularized_item(self):
         return self.alpha * (self.r * np.sign(self.theta) + (1-self.r) * self.theta)
