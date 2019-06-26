61d84431e133617cdff8c7f5513556747b23419b
two <dtcf@163.com>
2019-06-26 12:03:50 +0800

    add mnist test case

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
+from Cython.Build import cythonize
+from Cython.Compiler import Options
+import numpy
+
+"""
+    ~$: python setup.py bdist_egg
+
+    manually build Cython extension
+    ~$: python setup.py build_ext -if
+"""
+
+# set False while developing
+Options.annotate = True
+
+extensions = [
+    Extension(
+        # path of .so
+        "supervised.regression_tree",
+        ["supervised/regression_tree.pyx"],
+        # include_dirs=['/some/path/to/include/'],
+        # libraries=['ext_C_libs'],
+        # library_dirs=['/some/path/to/include/'],
+    ),
+]
+
+setup(
+    name='ml-toys',
+    version='0.9',
+    description='a tiny lib of machine learning',
+    author='zyf',
+    author_email='dtcf@163.com',
+    url='https://github.com/two2er/ml-toys',
+    packages=find_packages(),
+
+    setup_requires=['numpy', 'cython'],
+
+    ext_modules=cythonize(extensions,
+                          # path of generated C files
+                          build_dir='build'),
+    include_dirs=[numpy.get_include()],
+)
--- supervised/perceptron.py
+++ supervised/perceptron.py
@@ -16,6 +16,9 @@ class Perceptron:
         self.learning_rate = learning_rate
         if n_epoch == -1:
             self.n_epoch = 0x7fffffff
+        else:
+            self.n_epoch = n_epoch
+        self.theta = None
 
     def fit(self, X, y):
         """
@@ -23,26 +26,48 @@ class Perceptron:
         """
         # add x_0 = 1 for samples
         X_b = np.insert(X, 0, 1, axis=1)
+        del X
         # theta: randomly initialized: [-1/sqrt(n), 1/sqrt(n)]
         limit = np.sqrt(X_b.shape[1])
         self.theta = np.random.uniform(-1 / limit, 1 / limit, (X_b.shape[1], 1))
 
         for epoch in range(self.n_epoch):
             # collect misclassified samples in each iteration
-            pred = np.sign(X_b.dot(self.theta)).flatten()
-            X_b_mis = X_b[pred != y]
+            mis_index = (y * X_b.dot(self.theta) <= 0).flatten()
+            X_b_mis = X_b[mis_index]
             if len(X_b_mis) == 0:
                 # no misclassified samples any more
                 break
-            y_mis = y[pred != y]
-            print('loss:', self._loss(X_b_mis, y_mis))
+            y_mis = y[mis_index]
+            print('epoch: {}. loss (distance of misclassified samples): {}'.format(epoch,
+                                                                                   self._loss(X_b_mis, y_mis)))
+
             # update theta
-            for i in range(len(X_b_mis)):
-                self.theta += self.learning_rate * y_mis[i] * np.expand_dims(X_b_mis[i], axis=1)
+            self.theta += self.learning_rate * np.expand_dims(np.sum(y_mis * X_b_mis, axis=0), axis=1)
+            del X_b_mis, y_mis, mis_index
 
     def predict(self, X):
         X_b = np.insert(X, 0, 1, axis=1)
-        return np.sign(X_b.dot(self.theta)).flatten()
+        return np.sign(X_b.dot(self.theta))
 
     def _loss(self, X_mis, y_mis):
-        return -np.sum(y_mis * (X_mis.dot(self.theta).flatten()))
+        return -np.sum(y_mis * (X_mis.dot(self.theta)))
+
+
+if __name__ == '__main__':
+    import pandas as pd
+    from sklearn.model_selection import train_test_split
+    dataset = pd.read_csv('../dataset/mnist.csv', header=0).values
+
+    X = dataset[:, 1:]
+    y = dataset[:, 0]
+    y[y == 0] = -1
+    y = np.expand_dims(y, axis=1).astype(np.float64)
+
+    train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.3, random_state=40)
+
+    p = Perceptron(learning_rate=0.1e-4, n_epoch=20)
+    p.fit(train_X, train_y)
+
+    pred = p.predict(test_X)
+    print("accuracy: ", np.sum(pred == test_y)/len(test_y))
